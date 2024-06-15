096803957b20dbb3864c01e1d9f3a0874e4480d1
Oscar Otero <oom@oscarotero.com>
2024-06-15 15:41:20 +0200

    use simpleIcons plugin

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and this
 project adheres to [Semantic Versioning](http://semver.org/).
 
+## [0.1.4] - 2024-06-15
+### Changed
+- Use `lume_icon_plugins/simpleicons` to load the logos.
+
 ## [0.1.3] - 2024-03-29
 ### Changed
 - Use import map for LumeCMS.
@@ -24,6 +28,7 @@ project adheres to [Semantic Versioning](http://semver.org/).
 ## [0.1.0] - 2024-03-20
 First version
 
+[0.1.4]: https://github.com/lumeland/theme-simple-me/compare/v0.1.3...v0.1.4
 [0.1.3]: https://github.com/lumeland/theme-simple-me/compare/v0.1.2...v0.1.3
 [0.1.2]: https://github.com/lumeland/theme-simple-me/compare/v0.1.1...v0.1.2
 [0.1.1]: https://github.com/lumeland/theme-simple-me/compare/v0.1.0...v0.1.1
--- plugins.ts
+++ plugins.ts
@@ -1,14 +1,11 @@
 import "lume/types.ts";
 import Color from "https://colorjs.io/dist/color.js";
+import simpleIcons from "https://deno.land/x/lume_icon_plugins@v0.1.1/simpleicons.ts";
 import basePath from "lume/plugins/base_path.ts";
 import favicon from "lume/plugins/favicon.ts";
 import metas from "lume/plugins/metas.ts";
 import postcss from "lume/plugins/postcss.ts";
 import transformImages from "lume/plugins/transform_images.ts";
-import * as si from "npm:simple-icons@11.9.0";
-import type { SimpleIcon } from "npm:simple-icons@11.9.0";
-
-const icons = Object.values(si) as SimpleIcon[];
 
 /** Configure the site */
 export default function () {
@@ -18,12 +15,8 @@ export default function () {
       .use(favicon())
       .use(basePath())
       .mergeKey("extra_head", "stringArray")
-      .use(transformImages());
-
-    site.data("icon", (slug?: string) => {
-      if (!slug) return;
-      return icons.find((icon) => icon.slug === slug);
-    });
+      .use(transformImages())
+      .use(simpleIcons());
 
     site.data("textColor", (hex: string) => {
       const color = new Color(`#${hex}`);
